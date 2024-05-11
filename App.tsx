3fa94dbfd6a8fe2ea61fc872e73b31d008520b75
ScaferuZ <endrafruz@gmail.com>
2024-05-11 21:25:39 +0700

    feat: wrap navigation in a safe area provider

--- App.tsx
+++ App.tsx
@@ -3,12 +3,16 @@ import Button from "components/Button";
 import { StatusBar } from "expo-status-bar";
 import Navigation from "navigation";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import theme from "utils/theme";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Navigation />
+      <SafeAreaProvider>
+        <Navigation />
+        <StatusBar translucent />
+      </SafeAreaProvider>
     </ThemeProvider>
   );
 }
