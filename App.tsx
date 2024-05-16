f0812b0411af480a2d446f92812c5d44b4a22d0e
ScaferuZ <endrafruz@gmail.com>
2024-05-16 11:15:22 +0700

    feat: adding userGlobalStore for storing user data using zustand

--- App.tsx
+++ App.tsx
@@ -2,8 +2,10 @@ import { ThemeProvider } from "@shopify/restyle";
 import Button from "components/Button";
 import { StatusBar } from "expo-status-bar";
 import Navigation from "navigation";
+import { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import useUserGlobalStore from "store/useUserGlobalStore";
 import theme from "utils/theme";
 
 export default function App() {
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,14 +1,26 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import AuthStackNavigator from "./auth-stack-navigator";
 import { NavigationContainer } from "@react-navigation/native";
 import AppStackNavigator from "./app-stack-navigator";
+import useUserGlobalStore from "store/useUserGlobalStore";
 
 const Navigation = () => {
+  const { user, updateUser } = useUserGlobalStore();
+
+  console.log(`user`, JSON.stringify(user, null, 2));
+
+  // useEffect(() => {
+  //   updateUser({
+  //     email: "aa@gmail.com",
+  //     name: "aa"
+  //   });
+
+  //   return () => {};
+  // }, []);
   return (
     <NavigationContainer>
-      {/* <AuthStackNavigator /> */}
-      <AppStackNavigator />
+      {user ? <AppStackNavigator /> : <AuthStackNavigator />}
     </NavigationContainer>
   );
 };
